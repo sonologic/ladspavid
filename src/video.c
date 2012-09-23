5c2918d350a3aa979a45694214e6be736b177680
Koen Martens <gmc@sonologic.nl>
2012-09-23 19:16:10 +0200

    - clean up

--- src/video.c
+++ src/video.c
@@ -63,7 +63,7 @@ main (int   argc,
 {
   GMainLoop *loop;
 
-  GstElement *pipeline, *source, *decoder, *color, *freezer, *ladspa, *sink;
+  GstElement *pipeline, *pipeline2, *source, *decoder, *color, *freezer, *ladspa, *sink, *sink2;
   GstBus *bus;
 
   /* Initialisation */
@@ -81,13 +81,22 @@ main (int   argc,
 
   /* Create gstreamer elements */
   pipeline = gst_pipeline_new ("ladspavid");
+  pipeline2 = gst_pipeline_new ("ladspavid2");
   //source   = gst_element_factory_make ("filesrc",       "file-source");
   source   = gst_element_factory_make ("videotestsrc",       "video-test-source");
+  if(!source) { printf("unable to create source\n"); exit(-1); }
   decoder  = gst_element_factory_make ("decodebin",     "image-decoder");
+  if(!decoder) { printf("unable to create decoder\n"); exit(-1); }
   color    = gst_element_factory_make ("ffmpegcolorspace",     "color-spacer");
+  if(!color) { printf("unable to create color\n"); exit(-1); }
   freezer  = gst_element_factory_make ("imagefreeze",     "image-freezer");
-  ladspa   = gst_element_factory_make ("ladspavid", "ladspa-vid");
+  if(!freezer) { printf("unable to create freezer\n"); exit(-1); }
+  ladspa   = gst_element_factory_make ("ladspavidflanger", "ladspa-vid");
+  if(!ladspa) { printf("unable to create ladspa\n"); exit(-1); }
   sink     = gst_element_factory_make ("autovideosink", "video-output");
+  if(!sink) { printf("unable to create sink\n"); exit(-1); }
+  sink2    = gst_element_factory_make ("autovideosink", "video-output-2");
+  if(!sink2) { printf("unable to create sink2\n"); exit(-1); }
 
   if (!pipeline || !source || !decoder || !color || !freezer || !ladspa || !sink) {
     g_printerr ("One element could not be created. Exiting.\n");
@@ -97,11 +106,14 @@ main (int   argc,
   /* Set up the pipeline */
 
   /* we set the input filename to the source element */
-  g_object_set (G_OBJECT (source), "location", argv[1], NULL);
-  g_object_set (G_OBJECT (ladspa), "silent", 0, NULL);
+  //g_object_set (G_OBJECT (source), "location", argv[1], NULL);
+  //g_object_set (G_OBJECT (ladspa), "silent", 0, NULL);
 
   /* we add a message handler */
   bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
+  gst_bus_add_watch (bus, bus_call, loop);
+  gst_object_unref (bus);
+  bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline2));
   gst_bus_add_watch (bus, bus_call, loop);
   gst_object_unref (bus);
 
@@ -112,6 +124,8 @@ main (int   argc,
 
   gst_bin_add_many (GST_BIN (pipeline),
                     source, ladspa, sink, NULL);
+  gst_bin_add_many (GST_BIN (pipeline2),
+                    source, sink2, NULL);
 
   /* we link the elements together */
   /* file-source -> ogg-demuxer ~> vorbis-decoder -> converter -> alsa-output */
@@ -119,6 +133,8 @@ main (int   argc,
   //gst_element_link_many (color, freezer, ladspa, sink, NULL);
   //g_signal_connect (decoder, "pad-added", G_CALLBACK (on_pad_added), color);
   gst_element_link_many (source, ladspa, sink, NULL);
+  gst_element_link_many (source, sink2, NULL);
+  //gst_element_link_many (source, sink2, NULL);
 
   /* note that the demuxer will be linked to the decoder dynamically.
      The reason is that Ogg may contain various streams (for example
@@ -131,6 +147,7 @@ main (int   argc,
   /* Set the pipeline to "playing" state*/
   g_print ("Now playing: %s\n", argv[1]);
   gst_element_set_state (pipeline, GST_STATE_PLAYING);
+  gst_element_set_state (pipeline2, GST_STATE_PLAYING);
 
 
   /* Iterate */
